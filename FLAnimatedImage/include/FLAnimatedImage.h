4c64d3f5816187aae40fd12b61d044847d7dba3e
Zhang Li <jay.zhang@happyelements.com>
2024-08-29 19:21:37 +0800

    feat: 升级版本号

--- FLAnimatedImage/include/FLAnimatedImage.h
+++ FLAnimatedImage/include/FLAnimatedImage.h
@@ -57,6 +57,7 @@ extern const NSTimeInterval kFLAnimatedImageDelayTimeIntervalMinimum;
 @property (nonatomic, strong, readonly) NSData *data; // The data the receiver was initialized with; read-only
 
 @property (nonatomic, assign) float speed;
+@property (nonatomic, assign) NSNumber speedNum;
 
 @end
 
